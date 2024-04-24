8502350f765c35c8fa470f3749fb63571efbc4d9
7667ro <750745rohit@gmail.com>
2024-04-24 14:22:17 +0530

    First Commit

---
+++ Hostel/client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCLeQfP9aIYtgGOC7Bk3PUZvXai8qXVfs",
+  authDomain: "mern-state-b665d.firebaseapp.com",
+  projectId: "mern-state-b665d",
+  storageBucket: "mern-state-b665d.appspot.com",
+  messagingSenderId: "98249465525",
+  appId: "1:98249465525:web:acf26ca1355c876453e8fc"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ Hostel/client/src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>Welcome to HostelHunter!</h1>
+      <p className='mb-4 text-slate-700'>At HostelHunter, we understand that finding the perfect hostel for your travels can make all the difference in your journey. Whether you're a solo traveler seeking new adventures, a group of friends exploring together, or a family on a budget-friendly vacation, HostelHunter is here to make your hostel search experience seamless and enjoyable.</p>
+      <p className='mb-4 text-slate-700'>
+       What sets HostelHunter apart is our dedication to providing comprehensive hostel listings, insightful reviews, and helpful travel tips to empower you in making informed decisions about your accommodations. Whether you're searching for a cozy hostel in the heart of a bustling city, a rustic retreat nestled in nature, or a vibrant hostel with a lively atmosphere, we've got you covered.
+      </p>
+      <p className='mb-4 text-slate-700'>With HostelHunter, you can:
+
+Explore a diverse range of hostels catering to every budget and preference.
+Read authentic reviews from fellow travelers to get insider tips and recommendations.
+Use our user-friendly search filters to narrow down your options based on location, price, amenities, and more.
+Book your stay directly through our platform with confidence and convenience.</p>
+    </div>
+  )
+}
