eed9f82d6c6bf5d927b61e2e5e8d2f7e6ab28267
Roshan Kamleshwar Singh <119474972+7667ro@users.noreply.github.com>
2024-04-28 21:02:37 +0530

    Update index.js

--- Hostel/api/index.js
+++ Hostel/api/index.js
@@ -18,7 +18,7 @@ mongoose
     console.log(err);
   });
 
-  const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -30,14 +30,17 @@ app.listen(3010, () => {
   console.log('Server is running on port 3010!');
 });
 app.use(cors());
-// app.use(express.static(path.join(__dirname, '/client/dist')));
+
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 
 
-
+app.get("*",(req,res) =>{
+  res.sendFile(path.join(__dirname, 'client', 'dist','index.html'));
+})
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
@@ -47,3 +50,4 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+
